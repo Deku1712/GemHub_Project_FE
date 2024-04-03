e76a790d5cbdf0df09748653fcf004f67a6a6b20
Deku1712 <tamtvnde160577@fpt.edu.vn>
2024-04-03 18:52:47 +0700

    finish lay out carousel and about

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from '../../components/Carousel'
+import About from '../../components/About'
+
+export default function HomePage() {
+    return (
+        <div>
+            <Carousel />
+            <About/>
+        </div>
+    )
+}
