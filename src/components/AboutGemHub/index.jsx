325118fe237ca1f1b57c317eca0ea36bcb8b1863
Deku1712 <tamtvnde160577@fpt.edu.vn>
2024-04-05 18:38:55 +0700

    finished 90% feedback

--- src/components/AboutGemHub/index.jsx
+++ src/components/AboutGemHub/index.jsx
@@ -84,7 +84,7 @@ export default function AboutGemHub() {
 
   return (
     <div className='w-full pb-[60px]'>
-      <h2 className='mt-[40px] mb-[40px] pt-[20px] border-t-[1px] border-black text-center text-[35px] text-brown uppercase font-SFUFuturaRegular font-semiBold'>
+      <h2 className='mt-[40px] mb-[40px] pt-[20px] border-t-[1px] border-black text-center text-[35px] text-brown uppercase tracking-widest font-SFUFuturaRegular font-semiBold'>
         <a href="#"> About GemHub</a>
       </h2>
       <div className='relative w-full mx-auto max-w-[800px] mb-[20px]'>
