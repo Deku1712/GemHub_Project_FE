ba03aed15e081b932add99162d73f9b0df0954ce
Deku1712 <tamtvnde160577@fpt.edu.vn>
2024-04-06 01:31:05 +0700

    footerandtotop

---
+++ src/api/instance.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+// Tạo một instance với các đối số truyền vào riêng biệt
+const baseURL = axios.create({
+  // Đường dẫn API dùng chung cho toàn bộ dự án
+  baseURL: 'http://localhost:3000/',
+
+  // Chỉ định dữ liệu truyền đi và từ máy chủ là dạng JSON.
+  headers: {
+    'Content-Type': 'application/json'
+
+  }
+})
+
+export default baseURL
